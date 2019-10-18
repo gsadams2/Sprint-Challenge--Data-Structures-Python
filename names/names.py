2ed50c642ec653a5f1b891d8f78d833eeda9b9ff
George Adams <georgeadams@DRs-MacBook-Pro.local>
2019-10-18 13:59:53 -0500

    another solution for names

--- names/names.py
+++ names/names.py
@@ -16,11 +16,17 @@ f.close()
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-duplicates = list(set(names_1) & set(names_2))
+# duplicates = list(set(names_1) & set(names_2))
 
 #& is the bitwise and.... compares each bit of the first operand to the corresponding bit of 
 # the second operand. If both bits are 1, the corresponding result bit is set to 1. Otherwise, 
 # the corresponding result bit is set to 0.
+
+
+duplicates = []
+for name in names_2:
+    if name in names_1:
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
