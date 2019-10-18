ec39caa90958b71dfddd40d31b50050ba3915cb6
George Adams <georgeadams@DRs-MacBook-Pro.local>
2019-10-18 12:24:51 -0500

    finished 1 and 2

--- names/names.py
+++ names/names.py
@@ -10,13 +10,22 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+duplicates = list(set(names_1) & set(names_2))
+
+#& is the bitwise and.... compares each bit of the first operand to the corresponding bit of 
+# the second operand. If both bits are 1, the corresponding result bit is set to 1. Otherwise, 
+# the corresponding result bit is set to 0.
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
+#Old runtime is 7.396 seconds 
 
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,17 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current < self.capacity:
+      self.storage[self.current] = item
+      self.current += 1 
+     
+    if self.current == self.capacity:
+      self.current = 0
 
   def get(self):
-    pass
+    arr_yo = []
+
+    for i in self.storage:
+      if i:
+        arr_yo.append(i)
+    return arr_yo
